e728cc8488db5d7d88cc20e48b061a2227c2a7cb
akshit <106751693+Akshitbhati@users.noreply.github.com>
2022-10-31 18:30:00 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('input__feild')
+const addBtn = document.getElementById('input__button')
+const ul = document.querySelector('.todo')
+
+addBtn.addEventListener('click', function(){
+    const li = document.createElement('li')
+    li.innerText = input.value
+    li.setAttribute('class', 'li')
+    ul.appendChild(li)
+
+    const completeBtn = document.createElement('button')
+    const deleteBtn = document.createElement('button')
+    
+    deleteBtn.innerText = 'Delete'
+    completeBtn.innerText  = 'Complete'
+    
+    deleteBtn.setAttribute('class', 'delete')
+    completeBtn.setAttribute('class', 'complete')
+    
+    li.appendChild(deleteBtn)
+    li.appendChild(completeBtn)
+
+    if(input.value === ""){
+        alert('The Task Cannot be Empty')
+        ul.removeChild(li)
+    }
+    
+    deleteBtn.addEventListener('click', function(){
+        ul.removeChild(li)
+    })
+    completeBtn.addEventListener('click', function(){
+        li.style.textDecoration = 'line-through'
+        li.style.color = 'grey'
+    })
+    input.value = ""
+})
