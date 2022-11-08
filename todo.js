bc6f451effc0b93b718e6bd9b203f761100553a6
akshit <106751693+Akshitbhati@users.noreply.github.com>
2022-11-08 21:02:33 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -34,3 +34,38 @@ addBtn.addEventListener('click', function(){
     })
     input.value = ""
 })
+// To Trigger the event using the Enter Button
+input.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+    const li = document.createElement('li')
+    li.innerText = input.value
+    li.setAttribute('class', 'li')
+    ul.appendChild(li)
+
+    const completeBtn = document.createElement('button')
+    const deleteBtn = document.createElement('button')
+    
+    deleteBtn.innerText = 'Delete'
+    completeBtn.innerText  = 'Complete'
+    
+    deleteBtn.setAttribute('class', 'delete')
+    completeBtn.setAttribute('class', 'complete')
+    
+    li.appendChild(deleteBtn)
+    li.appendChild(completeBtn)
+
+    if(input.value === ""){
+        alert('The Task Cannot be Empty')
+        ul.removeChild(li)
+    }
+    
+    deleteBtn.addEventListener('click', function(){
+        ul.removeChild(li)
+    })
+    completeBtn.addEventListener('click', function(){
+        li.style.textDecoration = 'line-through'
+        li.style.color = 'grey'
+    })
+    input.value = ""
+}
+})
